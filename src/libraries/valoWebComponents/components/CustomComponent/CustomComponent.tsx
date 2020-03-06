a8e23360ed5148a4256436c73a8a89d92947611d
Elio Struyf <info@estruyf.be>
2020-03-06 14:55:38 +0100

    Removed interface

--- src/libraries/valoWebComponents/components/CustomComponent/CustomComponent.tsx
+++ src/libraries/valoWebComponents/components/CustomComponent/CustomComponent.tsx
@@ -1,22 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-export interface IObjectParam {
-  [key: string]: string;
-}
-
 export interface ICustomComponentProps {}
 
 export interface ICustomComponenState {}
 
 export class CustomComponent extends React.Component<ICustomComponentProps, ICustomComponenState> {
-
-  constructor(props: ICustomComponentProps) {
-    super(props);
-    this.state = {
-      showPanel: false
-    };
-  }
 
   public render() {
     return <h1>Hello Valo World!</h1>;
